791c0755df2b806a1e91274a4f9045179c6c84e8
Dima Sazonov <mister.mitya99@gmail.com>
2019-01-13 13:21:53 +0300

    first settings

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <SayFullName name="Dima" surname="Sazonov" link="https://vk.com/eva_ty" />
+        <SayFullName name="Dima" surname="Zubrik" link="https://vk.com/idaly_kard" />
+        <SayFullName name="Nikita" surname="Korolenok" link="https://vk.com/ameyuriringo" />
+        <SayFullName name="Shurek" surname="Kareev" link="https://vk.com/alexkareev" />
       </div>
     );
   }
 }
 
+function SayFullName(props) {
+  return (
+    <div>
+      <h1> Мое имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+    </div>
+  )
+}
 export default App;
