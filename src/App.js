1047dbd98e5cf41398024bfbb49a91162dd4dde2
Dima Sazonov <mister.mitya99@gmail.com>
2019-01-14 20:43:50 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './Components/Header';
+import Menu from './Components/Menu';
+import Main from './Components/Main';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import './App.css';
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Dima" surname="Sazonov" link="https://vk.com/eva_ty" />
-        <SayFullName name="Dima" surname="Zubrik" link="https://vk.com/idaly_kard" />
-        <SayFullName name="Nikita" surname="Korolenok" link="https://vk.com/ameyuriringo" />
-        <SayFullName name="Shurek" surname="Kareev" link="https://vk.com/alexkareev" />
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     );
   }
 }
 
-function SayFullName(props) {
-  return (
-    <div>
-      <h1> Мое имя {props.name}, фамилия - {props.surname}</h1>
-      <a href={props.link}> Ссылка на мой профиль </a>
-    </div>
-  )
-}
 export default App;
